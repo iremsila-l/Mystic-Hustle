258dcd906948d6ae86efe8e947cb54d8cb8f3a2c
İrem Sıla Sarma <119035725+iremsila-l@users.noreply.github.com>
2024-11-03 20:40:31 +0300

    - fix

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mail;
-using UnityEditor.VersionControl;
+// using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 using Firebase;
